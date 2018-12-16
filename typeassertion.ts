1b1bd28ae5692d19b30f31fa2fd329ae0fd0a8b8
miissra <deepak29mishra@gmail.com>
2018-12-16 13:55:53 +0530

    Typeassertion as typecaseting 2 ways

---
+++ typeassertion.ts
@@ -0,0 +1,8 @@
+let message;
+message='abc';
+
+//First Way angle Method
+var endswithC =(<string>message).endsWith('c');
+
+//Second Method 
+var alternativeMethod =(message as string).endsWith('c');
