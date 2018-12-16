accf232419fbc24b75c1db4039919ac1bae55fce
miissra <deepak29mishra@gmail.com>
2018-12-16 14:42:29 +0530

    Interface Inline

---
+++ interfacesinlineannotation.ts
@@ -0,0 +1,23 @@
+//Inline Annotation 
+
+let drawPoint=(point:{x:number,y:number})=>{
+
+}
+
+drawPoint({
+    x:1,
+    y:2
+})
+
+///////////////////////////////////////////////////////////////////////
+
+//Interfaces Implementation for clean code and declaration of the structure of the function
+
+interface Point{
+    x:number,
+    y:number
+}
+
+let IdrawPoint=(point:Point)=>{
+
+}
